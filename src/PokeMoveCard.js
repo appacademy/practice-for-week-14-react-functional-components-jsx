5e1683ac8bf68da31a95b85e1fef674941431c24
Ricky Cheung <96600317+WingNinCheung@users.noreply.github.com>
2022-06-14 10:58:26 -0700

    Done Done

---
+++ src/PokeMoveCard.js
@@ -0,0 +1,10 @@
+export function PokeMoveCard({ id, type, move, level }) {
+  return (
+    <li className="poke-move-card">
+      <h3>Move {id}</h3>
+      <h4 style={{ padding: 10 }}>{move.toUpperCase()}</h4>
+      <p>Type: {type}</p>
+      <p>Level: {level}</p>
+    </li>
+  );
+}
--- src/PokeMoves.js
+++ src/PokeMoves.js
@@ -1,4 +1,5 @@
 import { moves } from "./data";
+import { PokeMoveCard } from "./PokeMoveCard";
 
 function PokeMoves() {
   return (
@@ -6,9 +7,7 @@ function PokeMoves() {
       <h1>PokeMoves</h1>
       <ul>
         {moves.map((item) => (
-          <li key={item.id}>
-            {item.id}. {item.move}
-          </li>
+          <PokeMoveCard key={item.id} {...item} />
         ))}
       </ul>
     </div>

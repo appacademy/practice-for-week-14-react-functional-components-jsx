0f40975ab90eef1f1c0cd543aead50285f75b9d0
Kglines <65455404+Kglines@users.noreply.github.com>
2022-08-02 17:44:32 -0400

    Showcase css and update showcase

--- src/Showcase.js
+++ src/Showcase.js
@@ -1,4 +1,5 @@
 import bulbImage from "./images/bulbasaur.jpg";
+import "./Showcase.css";
 
 function Showcase() {
   const favPokemon = "Bulbasaur";
@@ -8,18 +9,13 @@ function Showcase() {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>{favPokemon}'s Showcase Component</h1>
       <img src={bulbImage} alt={favPokemon} />
       <h2>
         {favPokemon}'s type is{" "}
-        <span style={{ backgroundColor: "#00dd00", color: "#fff" }}>
-          {pokeCharacteristics.type}
-        </span>{" "}
-        and one of their moves is{" "}
-        <span style={{ backgroundColor: "#fff", color: "#00dd00" }}>
-          {pokeCharacteristics.move}
-        </span>
+        <span className="type">{pokeCharacteristics.type}</span> and one of
+        their moves is <span className="move">{pokeCharacteristics.move}</span>
       </h2>
     </div>
   );
